0ef1f0a17178aacc669432e2bba06965cc5545da
Bryan Nguyen <dongchirua@live.com>
2020-10-05 05:20:12 +0700

    CharacterEmbedding

--- models/embedding_net.py
+++ models/embedding_net.py
@@ -1,41 +1,81 @@
 import torch
 import torch.nn as nn
-from ultils.character_level import generate_char_embedding, CHAR_EMBEDDING_INDEX
-from torch.nn.utils.rnn import pad_sequence
+from torch.nn.utils.rnn import pack_padded_sequence, pad_packed_sequence
+import itertools
+import string
+
+from torch.autograd import Variable
 
 
-class EmbeddingNet(nn.Module):
-    def __init__(self, embedding_dim):
-        super(EmbeddingNet, self).__init__()
-        self.embeddings = generate_char_embedding(CHAR_EMBEDDING_INDEX, embedding_dim)
+class CharacterEmbedding(nn.Module):
+    def __init__(self, embedding_size):
+        super(CharacterEmbedding, self).__init__()
+        self.vocab = ['<pad>'] + list(string.printable)
+        self.embed = nn.Embedding(len(self.vocab), embedding_size)
+        self.is_cuda = False
+        self.cos = nn.CosineSimilarity(dim=2)
+
+    def flatten(self, l):
+        return list(itertools.chain.from_iterable(l))
+
+    def embedAndPack(self, seqs, batch_first=False):
+
+        vectorized_seqs = [[self.vocab.index(tok) for tok in seq] for seq in seqs]
+
+        # get the length of each seq in your batch
+        seq_lengths = torch.LongTensor(list(map(len, vectorized_seqs)))
+        seq_lengths = seq_lengths.cuda() if self.is_cuda else seq_lengths
+
+        # dump padding everywhere, and place seqs on the left.
+        # NOTE: you only need a tensor as big as your longest sequence
+        seq_tensor = Variable(torch.zeros((len(vectorized_seqs), seq_lengths.max()))).long()
+        seq_tensor = seq_tensor.cuda() if self.is_cuda else seq_tensor
+
+        for idx, (seq, seqlen) in enumerate(zip(vectorized_seqs, seq_lengths)):
+            seq_tensor[idx, :seqlen] = torch.LongTensor(seq).cuda() if self.is_cuda else torch.LongTensor(seq)
+
+        # SORT YOUR TENSORS BY LENGTH!
+        seq_lengths, perm_idx = seq_lengths.sort(0, descending=True)
+        seq_tensor = seq_tensor[perm_idx]
+
+        # utils.rnn lets you give (B,L,D) tensors where B is the batch size, L is the maxlength, if you use batch_first=True
+        # Otherwise, give (L,B,D) tensors
+        if not batch_first:
+            seq_tensor = seq_tensor.transpose(0, 1)  # (B,L,D) -> (L,B,D)
+
+        # embed your sequences
+        seq_tensor = self.embed(seq_tensor)
+
+        # pack them up nicely
+        return pack_padded_sequence(seq_tensor, seq_lengths.cpu().numpy(), batch_first=True)
+
+    def cuda(self):
+        self.is_cuda = True
+        self.embed = self.embed.cuda()
+        return self
 
     def forward(self, feed):
-        X, X_len = self.load_padded_data(feed, CHAR_EMBEDDING_INDEX)
-        return X, X_len
+        return self.embedAndPack(feed, batch_first=True)
 
-    def get_embedding(self, x):
-        return self.forward(x)
+    def unpackToSequence(self, packed_output):
+        output, _ = pad_packed_sequence(packed_output, batch_first=True)
+        words = self.unembed(output)
+        return words
 
-    def get_embedding_dim(self):
-        return self.embedding_dim
+    def unembed(self, embedded_sequence):
+        weights = self.embed.state_dict()['weight']
+        weights = weights.transpose(0, 1).unsqueeze(0).unsqueeze(0)
+        e_sequence = embedded_sequence.unsqueeze(3).data
+        cosines = self.cos(e_sequence, weights)
+        _, indexes = torch.topk(cosines, 1, dim=2)
 
-    def load_padded_data(self, feeds, word_to_index):
-        """
-        Padding data into a fixed length
-        :return: x_train_pad: padded data
-        :return: x_train_length: original length of all data (in case you want to unpadded)
-        """
-        x_train = []
-        for index, row in enumerate(feeds):
-            words_vector = [word_to_index.get(word) for word in row if word_to_index.get(word) is not None]
-            x_train.append(torch.LongTensor(words_vector))
-
-        x_train_len = [
-            len(x) for x in x_train
-        ]  # Get length for pack_padded_sequence after to remove padding
-        x_train_pad = pad_sequence(x_train, batch_first=True)
-        print("Load padded data successfully!")
-        return x_train_pad, x_train_len
+        words = []
+        for word in indexes:
+            word_l = ''
+            for char_index in word:
+                word_l += self.vocab[char_index[0]]
+            words.append(word_l)
+        return words
 
 
 if __name__ == '__main__':
@@ -43,14 +83,14 @@ if __name__ == '__main__':
     from dataset.augmentation import augment
 
     data = Inspectorio('~/Desktop/active_learning_data.xlsx', transform=augment)
-    print(data[1:3])
+    batch_data = data[1:10]
 
     embeddings_dim = 50
-    batch_size = 1
 
-    embed_net = EmbeddingNet(embeddings_dim)
-    out = embed_net(data[1])
-    print(out[0].shape)
-    print('#'*10)
-    out = embed_net(data[1:3])
-    print(out[0].shape)
+    embed_net = CharacterEmbedding(embeddings_dim)
+    out = embed_net(batch_data)
+    print(out)
+    print('#' * 10)
+
+    words = embed_net.unpackToSequence(out)
+    print(words)
