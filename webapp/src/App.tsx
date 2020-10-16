086fc088daf84f98aeacea1514a992f8f08a2700
Loc Vo <v.locvt3@vinbrain.net>
2020-10-16 16:28:48 +0700

    Render table

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -36,32 +36,37 @@ function App() {
           <button type="submit">Submit</button>
         </form>
       )}
-      {data?.map((entry, index) => (
-        <div key={index}>
-          <h3 style={{color: 'red'}}>{index}</h3>
-          <table style={{margin: "auto", marginBottom: 10, textAlign: 'left', width: 1000}}>
-            <thead>
-              <tr>
-                <th></th>
-                <th style={{width: '40%'}}>{entry?.a[0]}</th>
-                <th style={{width: '55%'}}>{entry?.a[1]}</th>
-              </tr>
-            </thead>
-            <tbody>
-              {entry?.b.map((subEntry: any[], subIndex: number) => (
-                <tr key={subIndex}>
-                  <td>
-                    <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
-                  </td>
-                  <td>{subEntry[0]}</td>
-                  <td>{subEntry[1]}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-          <br/>
-        </div>
-      ))}
+      {data.length > 0 && (
+        <>
+          {data?.map((entry, index) => (
+            <div key={index}>
+              <h3 style={{color: 'red'}}>{index}</h3>
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th style={{width: '40%'}}>{entry?.a[0]}</th>
+                    <th style={{width: '55%'}}>{entry?.a[1]}</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {entry?.b.map((subEntry: any[], subIndex: number) => (
+                    <tr key={subIndex}>
+                      <td>
+                        <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike" />
+                      </td>
+                      <td>{subEntry[0]}</td>
+                      <td>{subEntry[1]}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+              <br/>
+            </div>
+          ))}
+          <button type={"button"}>Done</button>
+        </>
+      )}
     </div>
   );
 }
